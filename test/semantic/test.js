e946ff38a7925459431fa3dc741ab963c2133708
Cameron Fleming <cameron@nevexo.space>
2020-12-11 15:21:08 +0000

    Updates to semantic highlighting

--- test/semantic/test.js
+++ test/semantic/test.js
@@ -1,5 +1,16 @@
 function test(param1) {
-    const variable = "test"
-    console.log(param1)
-    console.log(variable)
+    const unused_variable = "test"
+    const readonly_variable = "test"
+    let variable = "test"
+
+    console.log(param1)             // Function parameter
+    console.log(readonly_variable)  // Readonly variable (constant)
+    console.log(variable)           // Writable variable
+}
+
+class Test {
+    constructor() {
+        // nut
+        this._nut = "butter"
+    }
 }
